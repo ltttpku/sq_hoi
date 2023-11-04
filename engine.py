c53e8a87969a65c7c987d4b6e621eb8d6f4c32a2
ltttpku <ting_lei@pku.edu.cn>
2023-11-04 19:25:00 +0800

    add use_auxiliary_text

--- engine.py
+++ engine.py
@@ -96,7 +96,7 @@ def evaluate(model, postprocessors, criterion, data_loader, device, args):
         hoi_features = vision_outputs['hoi_features']
         hoi_features = hoi_features / hoi_features.norm(dim=-1, keepdim=True)
         logits_per_hoi = model.logit_scale.exp() * hoi_features @ text_features.t()
-        if args.use_auxiliary_texts:
+        if args.use_auxiliary_text:
             logits_per_hoi = logits_per_hoi + model.auxiliary_logit_scale.exp() * hoi_features @ auxiliary_text_features.t()
 
         pred_boxes = vision_outputs["pred_boxes"]
